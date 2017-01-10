7d3ca1135f4c64515d980ca3d44ce75867192ae8
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-01-10 19:03:31 +0000

    Updated to 1.1.0

--- lua/autorun/server/sv_commands.lua
+++ lua/autorun/server/sv_commands.lua
@@ -3,5 +3,8 @@ hook.Add( "PlayerSay", "OpenURLSelectMenu", function( ply, text, public )
 	if ( text == "/url" and ply:IsAdmin() ) then
 		ply:ConCommand("OpenURL_SelectMenu")
 		return ""
+	else
+		ply:SendLua(' chat.AddText( Color( 0, 255, 255 ), "You must be admin or higher to open this menu!") ')
+		return ""
 	end
 end )
