0952fb043dacaa55245d47b3c0a65fbb2aba41dd
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-01-12 21:55:18 +0000

    Updated to 1.1.2

--- lua/autorun/open_url.lua
+++ lua/autorun/open_url.lua
@@ -1,32 +1,37 @@
 local addonVersion = "1.1.2"
 
 if ( SERVER ) then
-    print("\nLoading OpenURL...")
-    print("  Author: viral32111")
-    print("  Issues: https://github.com/viral32111/openurl/issues")
-    print("  Steam: http://steamcommunity.com/id/viral32111")
-    print("  Contact: viral32111@hotmail.com")
-    print("  Wiki: https://github.com/viral32111/openurl/wiki")
+    print("Loading OpenURL...")
+    print(" Author: viral32111")
+    print(" Issues: https://github.com/viral32111/openurl/issues")
+    print(" Steam: http://steamcommunity.com/id/viral32111")
+    print(" Contact: viral32111@hotmail.com")
+    print(" Wiki: https://github.com/viral32111/openurl/wiki")
 
     http.Fetch( "https://raw.githubusercontent.com/viral32111/openurl/master/VERSION.md",
         function( body, len, headers, code )
             local formattedBody = string.gsub( body, "\n", "")
             if ( formattedBody == addonVersion ) then
-                print("You are running the most recent version of OpenURL (" .. formattedBody .. ")")
+                print(" You are running the most recent version of OpenURL (" .. formattedBody .. ")")
             else
-                print("You are using outdated version of OpenURL!\n Latest version: " .. formattedBody .. "\n Your version: " .. addonVersion )
+                print(" You are using outdated version of OpenURL!\n  Latest version: " .. formattedBody .. "\n  Your version: " .. addonVersion )
             end
         end,
         function( error )
-            print("Failed to Fetch HTTP")
+            print(" OpenURL Error: Failed to HTTP")
         end
     )
 
+    AddCSLuaFile("autorun/client/cl_web_menu.lua")
     include("autorun/client/cl_web_menu.lua")
+    AddCSLuaFile("autorun/client/cl_auth_menu.lua")
+    include("autorun/client/cl_auth_menu.lua")
+    AddCSLuaFile("autorun/client/cl_select_menu.lua")
     include("autorun/client/cl_select_menu.lua")
+    
     include("autorun/server/sv_commands.lua")
 
-    print("Finished loading OpenURL!\n")
+    print("Finished loading OpenURL!")
 end
 
 if ( CLIENT ) then
