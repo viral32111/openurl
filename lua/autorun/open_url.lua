4019aa9626e2d4c396c46054bc19a6cb6f5e2a1c
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-01-13 12:51:36 +0000

    Updated to 1.1.2

--- lua/autorun/open_url.lua
+++ lua/autorun/open_url.lua
@@ -3,24 +3,11 @@ local addonVersion = "1.1.2"
 if ( SERVER ) then
     print("Loading OpenURL...")
     print(" Author: viral32111")
+    print(" Version: " .. addonVersion)
     print(" Issues: https://github.com/viral32111/openurl/issues")
     print(" Steam: http://steamcommunity.com/id/viral32111")
     print(" Contact: viral32111@hotmail.com")
     print(" Wiki: https://github.com/viral32111/openurl/wiki")
-
-    http.Fetch( "https://raw.githubusercontent.com/viral32111/openurl/master/VERSION.md",
-        function( body, len, headers, code )
-            local formattedBody = string.gsub( body, "\n", "")
-            if ( formattedBody == addonVersion ) then
-                print(" You are running the most recent version of OpenURL (" .. formattedBody .. ")")
-            else
-                print(" You are using outdated version of OpenURL!\n  Latest version: " .. formattedBody .. "\n  Your version: " .. addonVersion )
-            end
-        end,
-        function( error )
-            print(" OpenURL Error: Failed to HTTP")
-        end
-    )
 
     AddCSLuaFile("autorun/client/cl_web_menu.lua")
     include("autorun/client/cl_web_menu.lua")
@@ -37,3 +24,19 @@ end
 if ( CLIENT ) then
     print("This server is using OpenURL (" .. addonVersion .. ") (Created by viral32111)")
 end
+
+hook.Add("OnGamemodeLoaded", "addonLoaded", function()
+	http.Fetch( "https://raw.githubusercontent.com/viral32111/openurl/master/VERSION.md",
+        function( body, len, headers, code )
+            local formattedBody = string.gsub( body, "\n", "")
+            if ( formattedBody == addonVersion ) then
+                print("You are running the most recent version of OpenURL (" .. formattedBody .. ")")
+            else
+                print("You are using outdated version of OpenURL! (Latest: " .. formattedBody .. ", Yours: " .. addonVersion .. ")" )
+            end
+        end,
+        function( error )
+            print("OpenURL Error: Failed to get version")
+        end
+    )
+end )
