77ca792d4a38bda7ab9f0ed1761e16ebd1df81ba
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-01-21 12:44:46 +0000

    Updated to 1.1.4

--- lua/autorun/openurl.lua
+++ lua/autorun/openurl.lua
@@ -38,12 +38,14 @@ hook.Add("PostGamemodeLoaded", "OpenURLLoad", function()
 	        local formattedBody = string.gsub( body, "\n", "")
 	        if ( formattedBody == addonVersion ) then
 	            print("[OpenURL] You are running the most recent version of OpenURL!")
+            elseif ( formattedBody == "404: Not Found" ) then
+                Error("[OpenURL] Error: Version page does not exist\n")
 	        else
 	            print("[OpenURL] You are using outdated version of OpenURL! (Latest: " .. formattedBody .. ", Yours: " .. addonVersion .. ")" )
 	        end
 	    end,
 	    function( error )
-	        print("[OpenURL] Error: Failed to get addon version")
+	        Error("[OpenURL] Error: Failed to get addon version\n")
 	    end
 	    )
 end )
