0fdee116237e04945e57f0d50ddb3cd54542962c
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-01-21 19:15:16 +0000

    Fixed indenting

--- lua/autorun/openurl.lua
+++ lua/autorun/openurl.lua
@@ -13,23 +13,23 @@ local function addFile( File, Type )
 end
 
 if ( SERVER ) then
-    print("[OpenURL] Loading OpenURL...")
-    print("[OpenURL] Author: viral32111")
-    print("[OpenURL] Version: " .. addonVersion )
+        print("[OpenURL] Loading OpenURL...")
+        print("[OpenURL] Author: viral32111")
+        print("[OpenURL] Version: " .. addonVersion )
 
-	util.AddNetworkString("OpenURLRequest")
+        util.AddNetworkString("OpenURLRequest")
 
-    addFile("sv_commands.lua", "server")
-    addFile("sv_request.lua", "server")
-    addFile("cl_webmenu.lua", "client")
-    addFile("cl_requestmenu.lua", "client")
-    addFile("cl_selectmenu.lua", "client")
+        addFile("sv_commands.lua", "server")
+        addFile("sv_request.lua", "server")
+        addFile("cl_webmenu.lua", "client")
+        addFile("cl_requestmenu.lua", "client")
+        addFile("cl_selectmenu.lua", "client")
 
-    print("[OpenURL] Finished loading OpenURL!")
+        print("[OpenURL] Finished loading OpenURL!")
 end
 
 if ( CLIENT ) then
-    print("[OpenURL] This server is using OpenURL! (Version: " .. addonVersion .. ") (Created by viral32111)")
+        print("[OpenURL] This server is using OpenURL! (Version: " .. addonVersion .. ") (Created by viral32111)")
 end
 
 hook.Add("PostGamemodeLoaded", "OpenURLLoad", function()
--- lua/autorun/server/sv_commands.lua
+++ lua/autorun/server/sv_commands.lua
@@ -1,14 +1,14 @@
 hook.Add("PlayerSay", "OpenURLSelectMenu", function( ply, text, public )
-	text = string.lower( text )
-	if ( text == "/url" ) then
-		if ( ply:IsAdmin() ) then
-			ply:ConCommand("openurlselect")
-			print("[OpenURL] " .. ply:Nick() .. " opened the selection menu")
-			return ""
-		else
-			ply:ChatPrint("You must be admin or higher to access this menu!")
-			print("[OpenURL] " .. ply:Nick() .. " tried to open the selection menu but failed.")
-			return ""
-		end
-	end
+        text = string.lower( text )
+        if ( text == "/url" ) then
+                if ( ply:IsAdmin() ) then
+                        ply:ConCommand("openurlselect")
+                        print("[OpenURL] " .. ply:Nick() .. " opened the selection menu")
+                        return ""
+                else
+                        ply:ChatPrint("You must be admin or higher to access this menu!")
+                        print("[OpenURL] " .. ply:Nick() .. " tried to open the selection menu but failed.")
+                        return ""
+                end
+        end
 end )
