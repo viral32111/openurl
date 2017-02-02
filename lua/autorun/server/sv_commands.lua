c4985e9f6ad0dae6825b286b0fe9d6c479159b2d
viral32111 <viral32111@users.noreply.github.com>
2017-02-02 21:15:02 +0000

    Selection menu is now usable by all usergroups

--- lua/autorun/server/sv_commands.lua
+++ lua/autorun/server/sv_commands.lua
@@ -6,15 +6,5 @@ hook.Add("PlayerSay", "openurlMenuCommand", function( ply, text, public )
 		ply:ConCommand("openurlselectionmenu")
 		print("[OpenURL] " .. ply:Nick() .. " opened the selection menu")
 	           return ""
-		--[[
-		if ( ply:IsAdmin() ) then
-	                      ply:ConCommand("openurlselectionmenu")
-	                      print("[OpenURL] " .. ply:Nick() .. " opened the selection menu")
-	                      return ""
-		else
-                       	ply:ChatPrint("You must be admin or higher to access this menu!")
-                       	print("[OpenURL] " .. ply:Nick() .. " tried to open the selection menu but failed.")
-                        	return ""
-                	end]]
         	end
 end )
