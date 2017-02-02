2203bfc8c78577f87006e5ac5e08fc397d0fb406
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-02-02 20:50:29 +0000

    Updated to 1.1.7

--- lua/autorun/openurl.lua
+++ lua/autorun/openurl.lua
@@ -1,6 +1,6 @@
 -- Copyright 2017 viral32111. https://github.com/viral32111/openurl/blob/master/LICENCE
 
-local addonVersion = "1.1.6"
+local addonVersion = "1.1.7"
 
 local function addFile( File, Type )
 	if ( Type == "server" ) then
--- lua/autorun/server/sv_versioncheck.lua
+++ lua/autorun/server/sv_versioncheck.lua
@@ -1,7 +1,7 @@
 -- Copyright 2017 viral32111. https://github.com/viral32111/openurl/blob/master/LICENCE
 
 versionchecked = false
-addonVersion = "1.1.6"
+addonVersion = "1.1.7"
 
 hook.Add("PlayerConnect", "openurlCheckVersion", function()
 	if not ( versionchecked ) then
