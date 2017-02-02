c84fc2840c7ddfcfa7ddadebdb7d1956f8ceba45
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-02-02 20:59:55 +0000

    Potential Bug Fix

--- lua/autorun/server/sv_commands.lua
+++ lua/autorun/server/sv_commands.lua
@@ -1,16 +1,18 @@
 -- Copyright 2017 viral32111. https://github.com/viral32111/openurl/blob/master/LICENCE
 
 hook.Add("PlayerSay", "openurlMenuCommand", function( ply, text, public )
-        text = string.lower( text )
-        if ( text == "/url" ) then
-                if ( ply:IsAdmin() ) then
-                        ply:ConCommand("openurlselectionmenu")                       
-                        print("[OpenURL] " .. ply:Nick() .. " opened the selection menu")
-                        return ""
-                else
-                        ply:ChatPrint("You must be admin or higher to access this menu!")
-                        print("[OpenURL] " .. ply:Nick() .. " tried to open the selection menu but failed.")
-                        return ""
-                end
-        end
+	text = string.lower( text )
+	if ( text == "/url" ) then
+		ply:ConCommand("openurlselectionmenu")
+		--[[
+		if ( ply:IsAdmin() ) then
+	                      ply:ConCommand("openurlselectionmenu")
+	                      print("[OpenURL] " .. ply:Nick() .. " opened the selection menu")
+	                      return ""
+		else
+                       	ply:ChatPrint("You must be admin or higher to access this menu!")
+                       	print("[OpenURL] " .. ply:Nick() .. " tried to open the selection menu but failed.")
+                        	return ""
+                	end]]
+        	end
 end )
